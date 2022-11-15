39b7161aa44093e436097ec92141dbf44bf19d09
donut-a <danuta.dopierala595@gmail.com>
2022-11-15 20:48:32 +0100

    3D-013: Save and cancel button

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     Resolution[] resolutions;
-    public GameObject resolutionDropdown;
+    public Dropdown resolutionDropdown;
+    public Toggle fullscreen, vsync;
     public void SetVolume(float volume) {
         audioMixer.SetFloat("Volume", volume);
         Debug.Log(volume);
@@ -27,21 +28,52 @@ public class SettingsMenu : MonoBehaviour
     }
     void Start()
     {
+        SetResolutions();
+        fullscreen.isOn = Screen.fullScreen;
+
+        if (QualitySettings.vSyncCount == 0)
+        {
+            vsync.isOn = false;
+        }
+        else {
+            vsync.isOn = true;
+        }
+
+    }
+
+    public void SetResolutions() {
         resolutions = Screen.resolutions;
-        Dropdown dropdown = resolutionDropdown.GetComponent<Dropdown>();
-        resolutionDropdown.GetComponent<Dropdown>().ClearOptions();
+        resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
         int currentIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++) {
+        for (int i = 0; i < resolutions.Length; i++)
+        {
             string item = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(item);
-            if (Screen.currentResolution.width == resolutions[i].width && Screen.currentResolution.height == resolutions[i].height) {
+            if (Screen.currentResolution.width == resolutions[i].width && Screen.currentResolution.height == resolutions[i].height)
+            {
                 currentIndex = i;
             }
         }
-        resolutionDropdown.GetComponent<Dropdown>().AddOptions(options);
-        resolutionDropdown.GetComponent<Dropdown>().value = currentIndex;
-        resolutionDropdown.GetComponent<Dropdown>().RefreshShownValue();
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
+    public void SaveSettings() {
+        if (vsync.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else { 
+            QualitySettings.vSyncCount = 0;
+        }
+        SetRes(resolutionDropdown.value);
+        SetFullScreen(fullscreen);
+
+    }
+
+    public void CancelSettings() {
+        SceneManager.LoadScene("MainMenu");
     }
 }
